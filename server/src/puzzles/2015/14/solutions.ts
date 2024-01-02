22f60bb10a2ad3f669fb63a4ee64b5ba34a52f56
Antoine <coulombe.antoine@hotmail.com>
2024-01-01 19:09:29 -0500

    Added solutions files

--- server/src/puzzles/2015/14/solutions.ts
+++ server/src/puzzles/2015/14/solutions.ts
@@ -1,119 +1,124 @@
-const RACE_DURATION = 2503;
+const puzzle_2015_14 = (part: number) => {
+	const RACE_DURATION = 2503;
 
-type Deer = {
-	name: string;
-	speed: number;
-	duration: number;
-	rest: number;
-	stars?: number;
-	totalDistance?: number;
-	remainingDuration?: number;
-	remainingRest?: number;
-};
+	type Deer = {
+		name: string;
+		speed: number;
+		duration: number;
+		rest: number;
+		stars?: number;
+		totalDistance?: number;
+		remainingDuration?: number;
+		remainingRest?: number;
+	};
 
-const deers: Deer[] = [
-	{
-		name: 'Rudolph',
-		speed: 22,
-		duration: 8,
-		rest: 165,
-	},
-	{
-		name: 'Cupid',
-		speed: 8,
-		duration: 17,
-		rest: 114,
-	},
-	{
-		name: 'Prancer',
-		speed: 18,
-		duration: 6,
-		rest: 103,
-	},
-	{
-		name: 'Donner',
-		speed: 25,
-		duration: 6,
-		rest: 145,
-	},
-	{
-		name: 'Dasher',
-		speed: 11,
-		duration: 12,
-		rest: 125,
-	},
-	{
-		name: 'Comet',
-		speed: 21,
-		duration: 6,
-		rest: 121,
-	},
-	{
-		name: 'Blitzen',
-		speed: 18,
-		duration: 3,
-		rest: 50,
-	},
-	{
-		name: 'Vixen',
-		speed: 20,
-		duration: 4,
-		rest: 75,
-	},
-	{
-		name: 'Dancer',
-		speed: 7,
-		duration: 20,
-		rest: 119,
-	},
-];
+	const deers: Deer[] = [
+		{
+			name: 'Rudolph',
+			speed: 22,
+			duration: 8,
+			rest: 165,
+		},
+		{
+			name: 'Cupid',
+			speed: 8,
+			duration: 17,
+			rest: 114,
+		},
+		{
+			name: 'Prancer',
+			speed: 18,
+			duration: 6,
+			rest: 103,
+		},
+		{
+			name: 'Donner',
+			speed: 25,
+			duration: 6,
+			rest: 145,
+		},
+		{
+			name: 'Dasher',
+			speed: 11,
+			duration: 12,
+			rest: 125,
+		},
+		{
+			name: 'Comet',
+			speed: 21,
+			duration: 6,
+			rest: 121,
+		},
+		{
+			name: 'Blitzen',
+			speed: 18,
+			duration: 3,
+			rest: 50,
+		},
+		{
+			name: 'Vixen',
+			speed: 20,
+			duration: 4,
+			rest: 75,
+		},
+		{
+			name: 'Dancer',
+			speed: 7,
+			duration: 20,
+			rest: 119,
+		},
+	];
 
-const initDeers = () => {
-	deers.forEach((deer) => {
-		deer.totalDistance = 0;
-		deer.remainingDuration = deer.duration;
-		deer.remainingRest = deer.rest;
-		deer.stars = 0;
-	});
-};
-
-const getMax = (deers: Deer[], key: string) => {
-	let furthest = deers[0];
-	deers.forEach((deer) => {
-		if ((deer as any)[key] > (furthest as any)[key]) {
-			furthest = deer;
-		}
-	});
-	return furthest;
-};
-
-const race = () => {
-	initDeers();
-	for (let i = 0; i < RACE_DURATION; ++i) {
+	const initDeers = () => {
 		deers.forEach((deer) => {
-			if (deer.remainingDuration! > 0) {
-				deer.totalDistance! += deer.speed;
-				--deer.remainingDuration!;
-			} else if (deer.remainingRest === 0) {
-				deer.remainingDuration = deer.duration - 1;
-				deer.remainingRest = deer.rest;
-				deer.totalDistance! += deer.speed;
-			} else {
-				--deer.remainingRest!;
+			deer.totalDistance = 0;
+			deer.remainingDuration = deer.duration;
+			deer.remainingRest = deer.rest;
+			deer.stars = 0;
+		});
+	};
+
+	const getMax = (deers: Deer[], key: string) => {
+		let furthest = deers[0];
+		deers.forEach((deer) => {
+			if ((deer as any)[key] > (furthest as any)[key]) {
+				furthest = deer;
 			}
 		});
+		return furthest;
+	};
 
-		const furthest = getMax(deers, 'totalDistance');
-		++furthest.stars!;
-	}
-};
+	const race = () => {
+		initDeers();
+		for (let i = 0; i < RACE_DURATION; ++i) {
+			deers.forEach((deer) => {
+				if (deer.remainingDuration! > 0) {
+					deer.totalDistance! += deer.speed;
+					--deer.remainingDuration!;
+				} else if (deer.remainingRest === 0) {
+					deer.remainingDuration = deer.duration - 1;
+					deer.remainingRest = deer.rest;
+					deer.totalDistance! += deer.speed;
+				} else {
+					--deer.remainingRest!;
+				}
+			});
 
-const part1 = () => {
-	race();
-	return getMax(deers, 'totalDistance');
-};
+			const furthest = getMax(deers, 'totalDistance');
+			++furthest.stars!;
+		}
+	};
 
-const part2 = () => {
-	race();
-	return getMax(deers, 'stars');
+	const part1 = () => {
+		race();
+		return getMax(deers, 'totalDistance');
+	};
+
+	const part2 = () => {
+		race();
+		return getMax(deers, 'stars');
+	};
+
+	if (part === 1) return part1();
+	if (part === 2) return part2();
 };
---
+++ server/src/puzzles/2017/14/solutions.ts
@@ -0,0 +1,11 @@
+const puzzle_2023_14 = (part: number) => {
+	const part1 = () => {
+		return '1';
+	};
+	const part2 = () => {
+		return '2';
+	};
+
+	if (part === 1) return part1();
+	if (part === 2) return part2();
+};
